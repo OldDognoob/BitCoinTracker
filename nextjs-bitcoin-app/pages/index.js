4e2492f0da65325e177a4846cb078d7af2e23664
Dimos-Christidis <christidis.dimosthenis@gmail.com>
2021-05-04 19:29:04 +0300

    wip

--- nextjs-bitcoin-app/pages/index.js
+++ nextjs-bitcoin-app/pages/index.js
@@ -1,16 +1,15 @@
 import Head from 'next/head'
-import styles from '../styles/Home.module.css'
 import SearchBar from '../components/SearchBar';
 
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <div>
       <Head>
         <title>Title</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <SearchBar />
+      <SearchBar type="text" placeholder="Search"/>
     </div>
   )
 }
